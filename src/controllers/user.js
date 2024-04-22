58c365b5e4159b8d8d7bc86cbf1d798648a3bd4f
vanthanhctitsea <thanh1993.ctit@gmail.com>
2024-04-22 10:33:40 +0700

    User Registration and Authentication

---
+++ src/controllers/user.js
@@ -0,0 +1,83 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const UserModel = require('../models/user');
+
+const gennerateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d'});
+}
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        res.status(400);
+
+        throw new Error('Please add all fields !');
+    }
+
+    const userExists = await UserModel.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+
+        throw new Error('User already exists !');
+    }
+
+    const salt = await bcrypt.getSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await UserModel.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
+    if (user) {
+        res.status(201).json({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: gennerateToken(user.id)
+        })
+    } else {
+        res.status(400);
+
+        throw new Error('Invalid user data');
+    }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || password) {
+        res.status(400);
+
+        throw new Error('Please add all fields');
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: gennerateToken(user.id)
+        })
+    } else {
+        res.status(400);
+
+        throw new Error('Invalid credentials');
+    }
+});
+
+const getLoggedInUser = asyncHandler(async (req, res) => {
+    const {id, name, email } = await UserModel.findById(req.user.id);
+
+    res.status(200).json({
+        id: id,
+        name,
+        email
+    })
+})
